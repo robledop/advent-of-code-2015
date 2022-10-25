7b41fec5ebe831459c0f14878adac6de7805ccee
Robledo Pazotto <robledo@gmail.com>
2022-10-25 10:46:25 -0700

    Day 2, part 2

--- day2/main.c
+++ day2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "sys/param.h"
 
 /*
     --- Day 2: I Was Told There Would Be No Math ---
@@ -23,6 +24,46 @@
     should they order?
 */
 
+/*
+    --- Part Two ---
+    The elves are also running low on ribbon. Ribbon is all the same width, so they only have
+    to worry about the length they need to order, which they would again like to be exact.
+
+    The ribbon required to wrap a present is the shortest distance around its sides, or the
+    smallest perimeter of any one face. Each present also requires a bow made out of ribbon as
+    well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume
+    of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+    For example:
+
+    A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present
+    plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+    A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present
+    plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+    How many total feet of ribbon should they order?
+*/
+
+int find_smallest(int a, int b, int c)
+{
+    if (a < b && a < c)
+    {
+        return a;
+    }
+    else if (b < c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
+
+int find_second_smallest(int a, int b, int c)
+{
+    return MAX(MIN(a, b), MIN(MAX(a, b), c));
+}
+
 int main(int argc, char **argv)
 {
     char *line = NULL;
@@ -31,8 +72,10 @@ int main(int argc, char **argv)
     text_file = fopen("input1.txt", "r");
 
     size_t total_paper_area = 0;
+    size_t total_ribbon_length = 0;
     while (getline(&line, &len, text_file) != -1)
     {
+        printf("%s", line);
         int length = atoi(strtok(line, "x"));
         int width = atoi(strtok(NULL, "x"));
         int height = atoi(strtok(NULL, "x"));
@@ -42,25 +85,20 @@ int main(int argc, char **argv)
         size_t area3 = (2 * height * length);
         size_t box_surface_area = area1 + area2 + area3;
 
-        int smallest_side = 0;
-
-        if (area1 < area2 && area1 < area3)
-        {
-            smallest_side = area1 / 2;
-        }
-        else if (area2 < area3)
-        {
-            smallest_side = area2 / 2;
-        }
-        else
-        {
-            smallest_side = area3 / 2;
-        }
+        int smallest_side = find_smallest(area1, area2, area3) / 2;
 
         size_t paper_area = box_surface_area + (smallest_side);
 
         total_paper_area += paper_area;
+
+        int shortest_edge = find_smallest(length, width, height);
+        printf("smallest edge: %d\n", shortest_edge);
+        int second_shortest_edge = find_second_smallest(length, width, height);
+        printf("second smallest edge: %d\n", second_shortest_edge);
+
+        total_ribbon_length += (2 * shortest_edge) + (2 * second_shortest_edge) + (length * height * width);
     }
 
     printf("Total paper area: %zu square feet\n", total_paper_area);
+    printf("Total ribbon length: %zu feet\n", total_ribbon_length);
 }
